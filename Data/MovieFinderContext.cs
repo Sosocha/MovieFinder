7b899f6ed684d5a4384baa4c5ea394076d9f973e
Charles Enesoso <enesosoc@gmail.com>
2021-04-27 22:09:43 -0400

    Add project files.

---
+++ Data/MovieFinderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieFinder.Model;
+
+namespace MovieFinder.Data
+{
+    public class MovieFinderContext : DbContext
+    {
+        public MovieFinderContext (DbContextOptions<MovieFinderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieFinder.Model.Movie> Movie { get; set; }
+    }
+}
---
+++ Model/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MovieFinder.Model
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]  // Specifies the type of data(date)
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+
+
+
+
+
+    }
+}
